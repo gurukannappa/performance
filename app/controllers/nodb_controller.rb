c6338b967efbf866f4132eba7c927689d2e8c08f
Guruprasad M <gurukannappa@gmail.com>
2018-01-17 14:31:56 +0530

    Maintaining different delay paths.

--- app/controllers/nodb_controller.rb
+++ app/controllers/nodb_controller.rb
@@ -1,6 +1,51 @@
 class NodbController < ApplicationController
 
   def just_json
-    render :json => {:message => "No DB calls made, No Page rendered. Just Json returned."}
+    render :json => {:message => controller_message }
+  end
+
+  def one_second_json
+    sleep 1
+    render :json => {:message => "#{controller_message} with 1 second delay"}
+  end
+
+  def two_second_json
+    sleep 2
+    render :json => {:message => "#{controller_message} with 2 second delay"}
+  end
+
+  def three_second_json
+    sleep 3
+    render :json => {:message => "#{controller_message} with 3 second delay"}
+  end
+
+  def four_second_json
+    sleep 4
+    render :json => {:message => "#{controller_message} with 4 second delay"}
+  end
+
+  def five_second_json
+    sleep 5
+    render :json => {:message => "#{controller_message} with 5 second delay"}
+  end
+
+  def thirty_second_json
+    sleep 30
+    render :json => {:message => "#{controller_message} with 30 second delay"}
+  end
+
+  def one_minute_json
+    sleep 60
+    render :json => {:message => "#{controller_message} with 60 second delay"}
+  end
+
+  def custom_second_json
+    sleep params[:id].to_i
+    render :json => {:message => "#{controller_message} with #{params[:id]} second delay"}
+  end
+
+  private
+  def controller_message
+    "No DB calls made, No Page rendered. Just Json returned"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,17 @@ Rails.application.routes.draw do
   resources :nodb , :only => :none do 
     collection do 
       get :just_json
+      get :one_second_json
+      get :two_second_json
+      get :three_second_json
+      get :four_second_json
+      get :five_second_json
+      get :thirty_second_json
+      get :one_minute_json
+    end
+
+    member do 
+      get :custom_second_json
     end
   end
 end
